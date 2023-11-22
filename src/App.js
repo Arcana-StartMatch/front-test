0e9fda2f89e02a6e7226fe4475cdfe6f67da0d3d
Lord-forgos <johan.rousseau39@gmail.com>
2023-11-22 10:06:09 +0100

    ajout du react router dom

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Caroussel from "./Atoms/Caroussel";
+import {Route, Routes,} from 'react-router-dom';
+
 function App() {
-  return (
-    <Caroussel></Caroussel>
- );
+    return (
+        <Routes>
+            <Route path="/" element={<Caroussel />} />
+        </Routes>
+    );
 }
 
 export default App;
