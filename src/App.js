2ca41d5922f41687a903c67721dc01db83512b44
Lord-forgos <johan.rousseau39@gmail.com>
2023-11-22 10:10:20 +0100

    add react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
 import Caroussel from "./Atoms/Caroussel";
-import {Route, Routes,} from 'react-router-dom';
+import {Route, Routes} from 'react-router-dom';
 
 function App() {
     return (
